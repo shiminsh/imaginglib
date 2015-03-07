6793ae4f21b60e48bdf24d99457c836f48b2b8bb
Shalini Roy <shaliniroy012@gmail.com>
2015-03-08 02:58:05 +0530

    added function to rotate images

--- resize.py
+++ resize.py
@@ -9,8 +9,27 @@ from PIL import Image
  
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
- 
- 
+
+def rotate(mypath, outputpath, degree):
+    outfile = os.path.join(outputpath, id_generator() + "." + imghdr.what(mypath) )
+    im = Image.open(mypath)
+    out = im.rotate(degree)
+    out.save(outfile) 
+
+def rotatelist(mypath, outputpath, degree):
+    onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+    print onlyfiles
+    onlyimagefile = []
+    for x in onlyfiles:
+        try:
+            Image.open(os.path.join(mypath, x))
+            onlyimagefile.append(x)
+        except:
+            pass
+    for y in onlyimagefile:
+        rotate(os.path.join(mypath,y), outputpath, degree)
+    print onlyimagefile
+
 def resizelist(mypath, outputpath, width, height):
     onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
     print onlyfiles
@@ -34,6 +53,10 @@ def resize(a, b, width, height):
  
  
 parser = argparse.ArgumentParser()
+parser.add_argument("-rz","--resize", help="image to be resize",
+                    action="store_true")
+parser.add_argument("-ro","--rotate", help="image to rotate",
+                    action="store_true")
 parser.add_argument("-p","--picture", help="display the resized image")
 parser.add_argument("-o","--output", help="image path")
 parser.add_argument("-d","--directory", help="path directory")
@@ -41,10 +64,17 @@ parser.add_argument("-w","--width", help="image width",
                      type=int)
 parser.add_argument("-i","--height", help="image height",
                     type=int)
+parser.add_argument("-deg","--degree", help="rotate image to a degree",
+                    type=int)
 args = parser.parse_args()
-if args.picture:
-    resize(args.picture, args.output, args.width, args.height)
-elif args.directory:
-    resizelist(args.directory, args.output, args.width, args.height)
-else:
-    print"oh no"
+if args.resize:
+    if args.picture:
+        resize(args.picture, args.output, args.width, args.height)
+    elif args.directory:
+        resizelist(args.directory, args.output, args.width, args.height)
+elif args.rotate:
+    if args.degree:
+        if args.picture:
+            rotate(args.picture, args.output, args.degree)
+        elif args.directory:
+            rotatelist(args.directory, args.output, args.degree)
