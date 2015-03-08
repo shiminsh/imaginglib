66f0ccdf5d82e4c8d148e181ba00b4ffe1179b28
Shalini Roy <shaliniroy012@gmail.com>
2015-03-08 15:44:46 +0530

    added cropping function

--- resize.py
+++ resize.py
@@ -10,6 +10,30 @@ from PIL import Image
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
+def crop(mypath, outputpath, left, upper, right, lower):
+    box = (left, upper, right, lower)
+    outfile = os.path.join(outputpath, id_generator() + "." + imghdr.what(mypath) )
+    im = Image.open(mypath)
+    try:
+        region = im.box(crop)
+        region.save(outfile)
+    except:
+        print "Enter proper coordinates"
+
+def croplist(mypath, outputpath, left, upper, right, lower):
+    onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
+    print onlyfiles
+    onlyimagefile = []
+    for x in onlyfiles:
+        try:
+            Image.open(os.path.join(mypath, x))
+            onlyimagefile.append(x)
+        except:
+            pass
+    for y in onlyimagefile:
+        crop(os.path.join(mypath,y), outputpath, left, upper, right, lower)
+    print onlyimagefile
+
 def extension(mypath, outputpath, convert):
     outfilepath = os.path.join(outputpath, id_generator() )
     f, e = os.path.splitext(mypath)
@@ -84,6 +108,8 @@ parser.add_argument("-ro","--rotate", help="image to rotate",
                     action="store_true")
 parser.add_argument("-con","--conversion", help="images to convert extension",
                     action="store_true")
+parser.add_argument("-crp","--cropping", help="image to be cropped",
+                    action="store_true")
 parser.add_argument("-p","--picture", help="display the resized image")
 parser.add_argument("-o","--output", help="image path")
 parser.add_argument("-d","--directory", help="path directory")
@@ -93,6 +119,14 @@ parser.add_argument("-w","--width", help="image width",
 parser.add_argument("-i","--height", help="image height",
                     type=int)
 parser.add_argument("-deg","--degree", help="rotate image to a degree",
+                    type=int)
+parser.add_argument("-l","--left", help="image to be crop from left",
+                    type=int)
+parser.add_argument("-up","--upper", help="image to be crop from upper",
+                    type=int)
+parser.add_argument("-r","--right", help="image to be crop from right",
+                    type=int)
+parser.add_argument("-lo","--lower", help="image to be crop from lower",
                     type=int)
 args = parser.parse_args()
 if args.resize:
@@ -112,3 +146,8 @@ elif args.conversion:
             extension(args.picture, args.output, args.convert)
         elif args.directory:
             convertlist(args.directory, args.output, args.convert)
+elif args.cropping:
+    if args.picture:
+        crop(args.picture, args.output, args.left, args.upper, args.right, args.lower)
+    elif args.directory:
+        croplist(args.directory, args.output, args.left, args.upper, args.right, args.lower)
